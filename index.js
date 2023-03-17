26d2e5dad5dce4b9d7c108ab4e0813f3ed8c12f0
Leushtean <tsepordei@gmail.com>
2023-03-17 23:13:40 +0000

    oper

--- index.js
+++ index.js
@@ -1,96 +1,112 @@
-/*
-let js = 'amazing';
-console.log(40+8+23-10);
+// let js = 'amazing';
+// console.log(40+8+23-10);
 
-console.log('Nicolae');
-console.log(23);
+// console.log('Nicolae');
+// console.log(23);
 
-let firstName = "Matilda";
+// let firstName = "Matilda";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
+// // Variable name conventions
 
-// Variable name conventions
-let jonas_matilda = "JM";
-let $function = 27;
-let person = "Nicolae";
-let PI = 3.1415;
+// let jonas_matilda = "JM";
+// let $function = 27;
+// let person = "Nicolae";
+// let PI = 3.1415;
 
-let myFirstJob = 'Coder';
-let myCurrentJob = 'Programmer';
-console.log(myCurrentJob);
+// let myFirstJob = 'Coder';
+// let myCurrentJob = 'Programmer';
+// console.log(myCurrentJob);
 
-//--=-=-=-=--=-=--=-=----=--=-=-=-=-=-=-=-=-=-=
+// //--=-=-=-=--=-=--=-=----=--=-=-=-=-=-=-=-=-=-=
 
-let country = 'Moldova';
-let continent = 'Europe';
-let population = '3 million';
+// let country = 'Moldova';
+// let continent = 'Europe';
+// let population = '3 million';
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-//-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-= D-A-T-A- T-Y-P-E
+// // -=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-= D-A-T-A- T-Y-P-E
 
-let isIsland = 'USA';
+// let isIsland = 'USA';
 
-let = isIsland = country;
-console.log(isIsland);
-*/
+// let = isIsland = country;
+// console.log(isIsland);
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
-console.log(typeof true);
-console.log(typeof javascriptIsFun);
-console.log(typeof javascriptIsFunnny);
-console.log(typeof 23);
-console.log(typeof 'Nicolae');
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFunnny);
+// console.log(typeof 23);
+// console.log(typeof "Nicolae");
 
-javascriptIsFun = 'YES!';
-console.log(javascriptIsFun);
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = "YES!";
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// year = 2023;
+// console.log(typeof year);
 
-year = 2023;
-console.log(typeof year);
+// year = "Year";
+// console.log(typeof year);
 
-year = 'Year';
-console.log(typeof year);
+// isIsland = true;
+// isIsland = "Moldova";
+// console.log(isIsland);
+// console.log(typeof isIsland);
 
-isIsland = true;
-isIsland = 'Moldova';
-console.log(isIsland);
-console.log(typeof isIsland);
+// let population;
+// population = 30000000;
+// console.log(population);
+// console.log(typeof population);
 
-let population;
-population = 30000000;
-console.log(population);
-console.log(typeof population);
+// country = true;
+// country = "Moldova";
+// console.log(country);
+// console.log(typeof country);
 
-country = true;
-country = 'Moldova'
-console.log(country);
-console.log(typeof country);
+// let lanquage;
+// console.log(lanquage);
+// console.log(typeof lanquage);
 
+// let lanquageRo;
+// console.log(lanquageRo);
+// console.log(typeof lanquageRo);
 
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- L=E=T C=O=N=S=T V=A=R
 
+// let age = 30;
+// age = 31;
 
-let lanquage;
-console.log(lanquage);
-console.log(typeof lanquage);
+// const birthYear = 1991;
+// // birthYear = 1234;
 
-let lanquageRo;
-console.log(lanquageRo);
-console.log(typeof lanquageRo);
+// var job = 'programmer';
+// job = 'teacher';
 
+// lastName ='Tepordei';
+// console.log(lastName);
 
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- B A S I C O P E R A T O R
+const now = 2023;
+const color = "red";
+const ageNicolae = now - 1975;
+const ageRodica = now - 1986;
+console.log(ageNicolae, ageRodica, color);
 
+console.log(ageNicolae * 2, ageRodica / 10, 2 ** 3);
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
-
-
+const firstName = "Nicolae";
+const lastName = "Tepordei";
+console.log(firstName + " " + lastName);
