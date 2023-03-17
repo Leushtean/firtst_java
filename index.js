256aa2809a657c6fc37fa0e82414dd507a2f0a7d
Leushtean <tsepordei@gmail.com>
2023-03-17 11:56:39 +0000

    first java

---
+++ index.js
@@ -0,0 +1,92 @@
+/*
+let js = 'amazing';
+console.log(40+8+23-10);
+
+console.log('Nicolae');
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+let person = "Nicolae";
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Programmer';
+console.log(myCurrentJob);
+
+//--=-=-=-=--=-=--=-=----=--=-=-=-=-=-=-=-=-=-=
+
+let country = 'Moldova';
+let continent = 'Europe';
+let population = '3 million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-= D-A-T-A- T-Y-P-E
+
+let isIsland = 'USA';
+
+let = isIsland = country;
+console.log(isIsland);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof javascriptIsFunnny);
+console.log(typeof 23);
+console.log(typeof 'Nicolae');
+
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2023;
+console.log(typeof year);
+
+year = 'Year';
+console.log(typeof year);
+
+isIsland = true;
+isIsland = 'Moldova';
+console.log(isIsland);
+console.log(typeof isIsland);
+
+let population;
+population = 30000000;
+console.log(population);
+console.log(typeof population);
+
+country = true;
+country = 'Moldova'
+console.log(country);
+console.log(typeof country);
+
+
+
+
+let lanquage;
+console.log(lanquage);
+console.log(typeof lanquage);
+
+
+
+
+
+
